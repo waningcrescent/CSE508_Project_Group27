305ba35aa6fe9319c2d0e25e390a985476841316
DevanshG1 <108400981+DevanshG1@users.noreply.github.com>
2024-04-23 19:48:56 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,9 @@
 import google.generativeai as genai
 
 def chatbot_response(user_text):
-    api_key='AIzaSyCrkcA_n7wp100fxfgrUwYK_jCIMLEt9jk'
+    api_key="AIzaSyCrkcA_n7wp100fxfgrUwYK_jCIMLEt9jk"
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(user_text)
     return response.text
 
